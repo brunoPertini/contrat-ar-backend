11c9af4e80a6d7ca86249d620e2201384af0e2c8
brunoPertini <brunopertini@gmail.com>
2023-06-13 10:12:38 -0300

    Cambios chicos

--- microservicio-gateway/src/main/java/com/contractar/microserviciogateway/security/SecurityConfig.java
+++ microservicio-gateway/src/main/java/com/contractar/microserviciogateway/security/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.oauth2.provider.expression.OAuth2WebSecurity
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-
 @Configuration
 public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
@@ -28,46 +27,41 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private RSAPrivateKey storePrivateKey;
-	
+
 	@Autowired
-    private OAuth2WebSecurityExpressionHandler expressionHandler;
-	
+	private OAuth2WebSecurityExpressionHandler expressionHandler;
+
 	@Bean
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
 
 	@Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
+	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 		converter.setKeyPair(new KeyPair(storePublicKey, storePrivateKey));
-				
-        return converter;
-    }
-	
+
+		return converter;
+	}
+
 	@Bean
 	public JwtDecoder jwtDecoder() {
-		 return NimbusJwtDecoder
-				 .withPublicKey(storePublicKey)
-				 .build();
+		return NimbusJwtDecoder.withPublicKey(storePublicKey).build();
 	}
-	
+
 	@Bean
-    public OAuth2WebSecurityExpressionHandler oAuth2WebSecurityExpressionHandler(ApplicationContext applicationContext) {
-        OAuth2WebSecurityExpressionHandler expressionHandler = new OAuth2WebSecurityExpressionHandler();
-        expressionHandler.setApplicationContext(applicationContext);
-        return expressionHandler;
-    }
-	
+	public OAuth2WebSecurityExpressionHandler oAuth2WebSecurityExpressionHandler(
+			ApplicationContext applicationContext) {
+		OAuth2WebSecurityExpressionHandler expressionHandler = new OAuth2WebSecurityExpressionHandler();
+		expressionHandler.setApplicationContext(applicationContext);
+		return expressionHandler;
+	}
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers(
-				"/actuator/**",
-				"/error")
-				.permitAll()
-				.antMatchers("/oauth/login").access("@securityUtils.hasValidClientId(request)")
-				.anyRequest()
+		http.authorizeRequests().antMatchers("/actuator/**", "/error").permitAll().antMatchers("/oauth/login")
+				.access("@securityUtils.hasValidClientId(request)").anyRequest()
 				.access("@securityUtils.hasValidClientId(request) and isAuthenticated()");
 
 		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
@@ -76,7 +70,7 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore());
