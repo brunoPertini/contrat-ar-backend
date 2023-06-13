30bdc89ae15199e96ac99df47c1238ae90bc7a85
brunoPertini <brunopertini@gmail.com>
2023-06-13 10:26:49 -0300

    Cambios chicos

--- microservicio-gateway/src/main/java/com/contractar/microserviciogateway/security/SecurityUtils.java
+++ microservicio-gateway/src/main/java/com/contractar/microserviciogateway/security/SecurityUtils.java
@@ -11,27 +11,25 @@ import org.springframework.stereotype.Component;
 
 @Component("securityUtils")
 public class SecurityUtils {
-	
+
 	@Value("${spring.security.oauth2.client.id}")
 	private String clientId;
-	
+
 	@Value("${spring.security.oauth2.client.secret}")
 	private String clientSecret;
 
 	public boolean hasValidClientId(HttpServletRequest request) {
-        String clientId = request.getHeader("client-id");
-        String clientSecret = request.getHeader("client-secret");
-        
-        boolean isValidClient = Optional.ofNullable(clientId)
-                .flatMap(cId -> Optional.ofNullable(clientSecret)
-                        .map(cSecret -> this.clientId.equals(cId) && this.clientSecret.equals(cSecret)))
-                .orElse(false);
-        
-        if (isValidClient) {
-        	return true;
-        } else {
-        	throw new OAuth2AuthenticationException(new OAuth2Error("Invalid client"));
-        }
+		String clientId = request.getHeader("client-id");
+		String clientSecret = request.getHeader("client-secret");
 
-    }
+		boolean isValidClient = Optional.ofNullable(clientId).flatMap(cId -> Optional.ofNullable(clientSecret)
+				.map(cSecret -> this.clientId.equals(cId) && this.clientSecret.equals(cSecret))).orElse(false);
+
+		if (isValidClient) {
+			return true;
+		} else {
+			throw new OAuth2AuthenticationException(new OAuth2Error("Invalid client"));
+		}
+
+	}
 }
