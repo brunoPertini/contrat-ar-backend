dce14b3ec31f81faffb73c52fedff4d824f17cea
brunoPertini <brunopertini@gmail.com>
2023-11-20 11:30:20 -0300

    Primera prueba de concepto ok

---
+++ microservicio-images/src/main/java/com/contractar/microservicioimages/MicroservicioImagesApplication.java
@@ -0,0 +1,13 @@
+package com.contractar.microservicioimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioImagesApplication.class, args);
+	}
+
+}
