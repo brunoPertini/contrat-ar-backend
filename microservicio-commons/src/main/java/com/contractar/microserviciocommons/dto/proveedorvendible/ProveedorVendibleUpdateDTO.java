92c363eb0381b3c02a7008b8b48fad1aebd605f0
brunoPertini <brunopertini@gmail.com>
2024-04-16 12:05:39 -0300

    Fixeando update de campos faltantes

--- microservicio-commons/src/main/java/com/contractar/microserviciocommons/dto/proveedorvendible/ProveedorVendibleUpdateDTO.java
+++ microservicio-commons/src/main/java/com/contractar/microserviciocommons/dto/proveedorvendible/ProveedorVendibleUpdateDTO.java
@@ -2,77 +2,71 @@ package com.contractar.microserviciocommons.dto.proveedorvendible;
 
 import org.locationtech.jts.geom.Point;
 
+import com.contractar.microserviciocommons.constants.PriceType.PriceTypeValue;
 import com.contractar.microserviciocommons.usuarios.UbicacionDeserializer;
 import com.contractar.microserviciocommons.usuarios.UbicacionSerializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 public class ProveedorVendibleUpdateDTO {
-	
+
 	private String descripcion;
 	private String imagenUrl;
 	private int precio;
+	private PriceTypeValue tipoPrecio;
+	
+	private boolean offersDelivery;
+	private boolean offersInCustomAddress;
+
 	private int stock;
 
 	@JsonDeserialize(using = UbicacionDeserializer.class)
 	@JsonSerialize(using = UbicacionSerializer.class)
 	private Point location;
 
-	public ProveedorVendibleUpdateDTO(String descripcion, String imagenUrl, int precio, int stock) {
+	public ProveedorVendibleUpdateDTO(String descripcion, String imagenUrl, int precio, int stock, PriceTypeValue tipoPrecio) {
 		this.descripcion = descripcion;
 		this.imagenUrl = imagenUrl;
 		this.precio = precio;
 		this.stock = stock;
+		this.tipoPrecio = tipoPrecio;
 	}
 
-	public ProveedorVendibleUpdateDTO() {}
+	public ProveedorVendibleUpdateDTO() {
+	}
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
-
 
 	public String getImagenUrl() {
 		return imagenUrl;
 	}
 
-
-
 	public void setImagenUrl(String imagenUrl) {
 		this.imagenUrl = imagenUrl;
 	}
-
-
 
 	public int getPrecio() {
 		return precio;
 	}
 
-
-
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
-
-
 
 	public int getStock() {
 		return stock;
 	}
 
-
-
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
-	
+
 	public Point getLocation() {
 		return location;
 	}
@@ -80,5 +74,28 @@ public class ProveedorVendibleUpdateDTO {
 	public void setLocation(Point location) {
 		this.location = location;
 	}
+
+	public PriceTypeValue getTipoPrecio() {
+		return tipoPrecio;
+	}
+
+	public void setTipoPrecio(PriceTypeValue tipoPrecio) {
+		this.tipoPrecio = tipoPrecio;
+	}
 	
+	public boolean isOffersDelivery() {
+		return offersDelivery;
+	}
+
+	public void setOffersDelivery(boolean offersDelivery) {
+		this.offersDelivery = offersDelivery;
+	}
+
+	public boolean isOffersInCustomAddress() {
+		return offersInCustomAddress;
+	}
+
+	public void setOffersInCustomAddress(boolean offersInCustomAddress) {
+		this.offersInCustomAddress = offersInCustomAddress;
+	}
 }
