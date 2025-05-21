bd8c352c009a25353a05c52c4608a92d4e6bc0b0
bruno <brunopertini@gmail.com>
2025-05-21 16:50:45 -0300

    Cambiando approach para autenticacion con el servicio de configuracion, primeas pruebas ok

---
+++ microservicio-config/src/main/java/com/contractar/microservicioconfig/security/ConfigServerSecurity.java
@@ -0,0 +1,20 @@
+package com.contractar.microservicioconfig.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ConfigServerSecurity {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
