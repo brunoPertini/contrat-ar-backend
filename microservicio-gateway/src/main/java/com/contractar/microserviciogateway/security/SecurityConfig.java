a6f7f194363a20e2c2d73fa6269daa71ba4a934e
bruno <40666255+brunoPertini@users.noreply.github.com>
2025-07-07 16:58:01 -0300

    Dejando  como publica url de consulta de pago

--- microservicio-gateway/src/main/java/com/contractar/microserviciogateway/security/SecurityConfig.java
+++ microservicio-gateway/src/main/java/com/contractar/microserviciogateway/security/SecurityConfig.java
@@ -153,6 +153,7 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 				.antMatchers(HttpMethod.PUT, clientesUrls).access(clientesOperationsAccsesRule)
 				.antMatchers(proveedorUrls).access(vendiblesOperationsAccsesRule)
 				.antMatchers(HttpMethod.GET, productosUrls[0]).hasAnyAuthority(proveedorProductoRole, clienteRole, adminRole)
+				.antMatchers(HttpMethod.GET, publicPayUrls).permitAll() // This is to let pass payment check in signup stage. TODO: do it through a better approach
 				.antMatchers(publicPayUrls).hasAnyAuthority(proveedorProductoRole, proveedorServicioRole, adminRole)
 				.antMatchers(HttpMethod.GET, passwordEmailUrls[1]).access("@securityUtils.tokenContainsType(request) and @securityUtils.hasValidClientId(request)")
 				.antMatchers(HttpMethod.GET, UsersControllerUrls.GET_USUARIO_INFO).access("@securityUtils.isAdminUser(request) or @securityUtils.userIdsMatch(request, \"usuarios\")")
